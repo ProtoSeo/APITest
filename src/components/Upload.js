2df98fc60c6ffb496ce802f2c953984d070690a0
SeoSeunghun <proto_seo@naver.com>
2021-09-27 22:45:24 +0900

    upload 기능 구현

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function Upload(){
-  const [formData, setFormData] = useState(new FormData());
-  const handleFormData = (e) => {
-    setFormData(e.target.files[0]);
-  };
-  const headers = {
-    "Content-Type": "multipart/form-data"
+  const [img,setImage] = useState(null)
+  const [imgUrl, setImgUrl] = useState("")
+  const handleImg = (e) => {
+    setImage(e.target.files[0]);
   };
   const onClickUpload = () => {
-    console.log("onclickUpload")
+    const formData = new FormData();
+    formData.append('data',img)
     axios
-      .post("http://localhost:8080/api/v1/upload",{
-        data:formData
-      },{headers})
-      .then((res) => console.log(res))
+      .post("http://localhost:8080/api/v1/upload",
+        formData
+      , {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res.data)
+        setImgUrl(res.data)
+      })
       .catch((errors) => console.log(errors));
   };
 
@@ -23,12 +29,16 @@ function Upload(){
     <div>
       <h2>Upload</h2>
       <div>
-        <input 
-          type="file"
-          onChange={handleFormData}
-          formEncType="multipart/form-data"
-          />
-        <button type="button" onClick={onClickUpload}>upload</button>
+        <form encType="multipart/form">
+          <input 
+            type="file"
+            onChange={handleImg}
+            />
+          <button type="button" onClick={onClickUpload}>upload</button>
+        </form>
+      </div>
+      <div>
+        <img src={imgUrl} alt="upload img"/>
       </div>
     </div>
   );
