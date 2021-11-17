bf236c8c5fe8f8a49b3566287e66bb9e9263c158
SeoSeunghun <proto_seo@naver.com>
2021-11-17 15:17:54 +0900

    [feat] api 연결

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -1,11 +1,12 @@
-import React, { useState }  from "react";
+import React, { useState, useEffect }  from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
-function Room(id){
+function Room({index, roomId, userId}){
   return (
     <div>
-      <Link to={{ pathname: "/meeting-room", state: { roomId: id } }}>
-        {id}
+      <Link key={index} to={{ pathname: "/meeting-room", state: { roomId: roomId, userId: userId } }}>
+        Room ID : {roomId}
       </Link>
     </div>
   );
@@ -13,29 +14,57 @@ function Room(id){
 function CreateRoom() {
   const [roomIds, setRoomIds] = useState([]);
   const [roomId, setRoomId] = useState(0);
+  const [userId, setUserId] = useState(0);
   
   const handleRoomId = (e) => {
     setRoomId(e.target.value)
   }
-
+  const handleUserId = (e) => {
+    setUserId(e.target.value)
+  }
   const addRoomId = () => {
+    axios
+      .post("http://localhost:8080/api/v1/rooms", null, { params:
+        {"room-id":roomId}
+      })
+      .then((res) => console.log(res))
+      .catch((errors) => console.log(errors));
     const set = new Set(roomIds);
     set.add(roomId);
     setRoomIds([...set]);
   };
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/v1/rooms")
+      .then((res) => {
+        setRoomIds(res.data)
+        console.log(res)
+      })
+      .catch((errors) => console.log(errors));
+  },[])
   return (
     <div>
       <div>
         <input
           type="number"
-          name="inputNumber"
+          name="inputRoomId"
           value={roomId}
           onChange={handleRoomId}
         />
         <button onClick={addRoomId}>Create Select Room Number</button>
       </div>
+      <hr/>
       <div>
-        {roomIds.map((a) =>Room(a))}
+        <label>UserID : </label>
+        <input
+          type="number"
+          name="inputUserId"
+          value={userId}
+          onChange={handleUserId}
+        />
+      </div>
+      <div>
+        {roomIds.map((roomId, index) => Room({index, roomId, userId}))}
       </div>
     </div>
   );
